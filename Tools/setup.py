6c0561028ac1665e6a024a7d627f062750439569
stickie <usethecorrectformat@gmail.com>
2023-11-29 15:47:30 +0800

    Remove spaces

--- Tools/setup.py
+++ Tools/setup.py
@@ -10,11 +10,13 @@ with open(root_path / 'Tools/template.py', 'r') as file:
 def make_dir(year: int):
     os.mkdir(root_path / f'{year}')
     for day in range(1, 26):
-        os.mkdir(root_path / f'{year}/Day {day}')
-        with open(root_path / f'{year}/Day {day}/day_{day}.py', 'w') as file:
+        os.mkdir(root_path / f'{year}/Day{day}')
+        with open(root_path / f'{year}/Day{day}/day_{day}.py', 'w') as file:
             file.write(template.format(day=day, year=year))
-        open(root_path / f'{year}/Day {day}/day_{day}.txt', 'x')
+        open(root_path / f'{year}/Day{day}/day_{day}.txt', 'x')
 
 
 if __name__ == '__main__':
-    pass
+    for year in range(2015, 2023):
+        for day in range(1, 26):
+            os.rename(root_path / f'{year}/Day {day}', root_path / f'{year}/Day{day}')
