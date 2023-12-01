f59bccfcff96eebb70665c5edaf95e21fa014520
stickie <usethecorrectformat@gmail.com>
2023-12-01 14:12:24 +0800

    day 1

--- Python/2023/Day01/code.py
+++ Python/2023/Day01/code.py
@@ -1,4 +1,5 @@
 from typing import TextIO
+import re
 
 from Python.path_stuff import *
 
@@ -7,13 +8,21 @@ def parser(raw_data: TextIO):
     return raw_data.read().splitlines()
 
 
-def part_a_solver(data):
-    return
+def part_a_solver(data: list[str]):
+    return sum(int(f'{nums[0]}{nums[-1]}') for nums in map(re.compile(r'\d').findall, data))
 
 
-def part_b_solver(data):
-    return 
-
+def part_b_solver(data: list[str]):
+    nums = 'zero|one|two|three|four|five|six|seven|eight|nine'
+    new_data = [
+        re.sub(
+            fr'(?=({nums}))',
+            lambda s: str(nums.split('|').index(s.group(1))),
+            line,
+        )
+        for line in data
+    ]
+    return part_a_solver(new_data)
 
 if __name__ == '__main__':
     testing = False
