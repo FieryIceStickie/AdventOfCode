5d8ae2144e938a5ce76381c34ad7866a67d2cdea
stickie <usethecorrectformat@gmail.com>
2023-12-03 00:36:17 +0800

    day 2 addendum - smarter parsing of input

--- Python/2023/Day02/code.py
+++ Python/2023/Day02/code.py
@@ -8,19 +8,17 @@ from typing import TextIO
 from Python.path_stuff import *
 
 
-def parser(raw_data: TextIO) -> list[list[Counter[str]]]:
-    return [[Counter({color: int(num) for num, color in bag})
-             for bag in map(re.compile(r'(\d+) (red|green|blue)').findall,
-                            line.split(';'))]
+def parser(raw_data: TextIO) -> list[Counter[str]]:
+    return [reduce(or_, [Counter({color: int(num)})
+                         for num, color in re.findall(r'(\d+) (\w+)', line)])
             for line in raw_data.read().splitlines()]
 
 
-def solver(games: list[list[Counter[str]]]) -> tuple[int, int]:
+def solver(games: list[Counter[str]]) -> tuple[int, int]:
     threshold = Counter({'red': 12, 'green': 13, 'blue': 14})
-    mega_bags = [reduce(or_, game) for game in games]
     return (
-        sum(idx for idx, bag in enumerate(mega_bags, start=1) if bag <= threshold),
-        sum(prod(bag.values()) for bag in mega_bags),
+        sum(idx for idx, bag in enumerate(games, start=1) if bag <= threshold),
+        sum(prod(bag.values()) for bag in games),
     )
 
 
