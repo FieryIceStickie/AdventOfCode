7640bfe7be25eb923db01d6f02d8e311f0721ab1
stickie <usethecorrectformat@gmail.com>
2023-12-04 23:23:52 +0800

    day 4 - fix type hints

--- Python/2023/Day04/code.py
+++ Python/2023/Day04/code.py
@@ -3,12 +3,12 @@ from typing import TextIO
 from Python.path_stuff import *
 
 
-def parser(raw_data: TextIO) -> list[list[set[int], set[int]]]:
+def parser(raw_data: TextIO) -> list[list[set[str], set[str]]]:
     parts = [line.split('|') for line in raw_data.read().splitlines()]
     return [[{*wins.split()[2:]}, {*hand.split()}] for wins, hand in parts]
 
 
-def solver(data: list[list[set[int], set[int]]]) -> tuple[int, int]:
+def solver(data: list[list[set[str], set[str]]]) -> tuple[int, int]:
     cards = [1] * len(data)
     points = 0
     copies = 0
