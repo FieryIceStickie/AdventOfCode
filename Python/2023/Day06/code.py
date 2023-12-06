e37f0b1c633393b8c16c1ca310ddc476fda026bb
stickie <usethecorrectformat@gmail.com>
2023-12-06 23:52:01 +0900

    day 6 - today was so trivial i did it in desmos

--- Python/2023/Day06/code.py
+++ Python/2023/Day06/code.py
@@ -1,18 +1,23 @@
 from typing import TextIO
+from math import prod, sqrt
 
 from Python.path_stuff import *
 
 
 def parser(raw_data: TextIO):
-    return raw_data.read().splitlines()
+    return [*zip(*[map(int, line.split()[1:]) for line in raw_data.read().splitlines()])]
 
 
 def part_a_solver(data):
-    return
+    return prod(
+        int(t/2 + sqrt(t*t-4*d)/2) - int(t/2 - sqrt(t*t-4*d)/2)
+        for t, d in data
+    )
 
 
 def part_b_solver(data):
-    return 
+    t, d = (int(''.join(map(str, c))) for c in zip(*data))
+    return int(t/2 + sqrt(t*t-4*d)/2) - int(t/2 - sqrt(t*t-4*d)/2)
 
 
 if __name__ == '__main__':
