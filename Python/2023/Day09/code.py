c856524240a5ecf983f00df096202877271f6805
stickie <usethecorrectformat@gmail.com>
2023-12-10 00:27:58 +0900

    day 9

--- Python/2023/Day09/code.py
+++ Python/2023/Day09/code.py
@@ -1,25 +1,30 @@
 from typing import TextIO
+from math import comb
+from itertools import pairwise, accumulate
 
 from Python.path_stuff import *
 
 
-def parser(raw_data: TextIO):
-    return raw_data.read().splitlines()
+def parser(raw_data: TextIO) -> list[list[int]]:
+    return [[*map(int, line.split())] for line in raw_data.read().splitlines()]
 
 
-def part_a_solver(data):
-    return
-
-
-def part_b_solver(data):
-    return 
+# noinspection PyTypeChecker
+def solver(data: list[list[int]]) -> tuple[int, int]:
+    return tuple(
+        sum(
+            sum(v[0] * ((-1)**k if mode else comb(len(line), k)) for k, v in enumerate(
+                accumulate([0] * (len(line) - 1), lambda seq, _: [j - i for i, j in pairwise(seq)], initial=line)))
+            for line in data
+        )
+        for mode in (0, 1)
+    )
 
 
 if __name__ == '__main__':
     testing = False
 
     with open(test_path if testing else 'input.txt', 'r') as file:
-        data = parser(file)
+        parsed_data = parser(file)
 
-    print(part_a_solver(data))
-    print(part_b_solver(data))
+    print(*solver(parsed_data))
