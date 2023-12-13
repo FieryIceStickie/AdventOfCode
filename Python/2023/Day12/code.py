1ef3aceed05c2aa5d11b7894e957faea1205028a
stickie <usethecorrectformat@gmail.com>
2023-12-13 14:16:23 +0900

    day 12

--- Python/2023/Day12/code.py
+++ Python/2023/Day12/code.py
@@ -1,25 +1,61 @@
 from typing import TextIO
 
 from Python.path_stuff import *
+from functools import cache
+
+type ParsedData = list[tuple[str, tuple[int]]]
 
 
-def parser(raw_data: TextIO):
-    return raw_data.read().splitlines()
+def parser(raw_data: TextIO) -> ParsedData:
+    return [
+        (
+            record,
+            (*map(int, groups.split(',')),)
+        )
+        for record, groups in map(str.split, raw_data.read().splitlines())
+    ]
 
 
-def part_a_solver(data):
-    return
+@cache
+def solve(record: str, groups: tuple[int], tracking: bool = False) -> int:
+    if not record:
+        return not any(groups)
+    elif not groups:
+        return all(c in '.?' for c in record)
+
+    elem = record[0]
+    if elem == '.':
+        if tracking:
+            if groups[0]:
+                return 0
+            new_groups = tuple(groups[1:])
+        else:
+            new_groups = groups
+        return solve(record[1:], new_groups, False)
+    elif elem == '#':
+        if not groups[0]:
+            return 0
+        *new_groups, = groups
+        new_groups[0] -= 1
+        new_groups = tuple(new_groups)
+        return solve(record[1:], new_groups, True)
+    elif elem == '?':
+        if tracking:
+            return solve(('#' if groups[0] else '.') + record[1:], groups, True)
+        return sum(solve(c + record[1:], groups, False) for c in '.#')
 
 
-def part_b_solver(data):
-    return 
+def solver(data: ParsedData) -> tuple[int, ...]:
+    return tuple(
+        sum(solve.cache_clear() or solve(*args) for args in pdata)
+        for pdata in (data, [('?'.join([record] * 5), groups * 5) for record, groups in data])
+    )
 
 
 if __name__ == '__main__':
     testing = False
 
     with open(test_path if testing else 'input.txt', 'r') as file:
-        data = parser(file)
+        parsed_data = parser(file)
 
-    print(part_a_solver(data))
-    print(part_b_solver(data))
+    print(*solver(parsed_data))
