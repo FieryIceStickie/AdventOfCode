e16aaed9efe91df1556c2d144e970ce3177c0e15
stickie <usethecorrectformat@gmail.com>
2023-12-19 14:53:59 +0900

    day 18

--- Python/2023/Day18/code.py
+++ Python/2023/Day18/code.py
@@ -1,18 +1,31 @@
 from typing import TextIO
+import re
 
 from Python.path_stuff import *
+from itertools import pairwise, accumulate
 
 
 def parser(raw_data: TextIO):
-    return raw_data.read().splitlines()
+    return [(1j**'DRUL'.find(direction), int(steps), color)
+            for direction, steps, color in re.findall(r'(\w) (\d+) \(#(\w{6})\)', raw_data.read())]
 
 
 def part_a_solver(data):
-    return
+    area = int(abs(sum((z1.conjugate() * z2).imag
+               for z1, z2 in pairwise(accumulate(data, lambda z, state: z + state[0]*state[1], initial=0)))) / 2)
+    boundary = sum(steps for _, steps, _ in data)
+    interior = area - boundary//2 + 1
+    return boundary + interior
 
 
 def part_b_solver(data):
-    return 
+    modified_data = [int(color[:5], 16) * 1j**int(color[-1]) for _, _, color in data]
+    area = int(abs(sum((z1.conjugate() * z2).imag
+                       for z1, z2 in
+                       pairwise(accumulate(modified_data, lambda z, d: z + d, initial=0)))) / 2)
+    boundary = int(sum(map(abs, modified_data)))
+    interior = area - boundary // 2 + 1
+    return boundary + interior
 
 
 if __name__ == '__main__':
